d6787e4244515764edd90603155b5b35a2c72eb7
Daniel Lubovich <daniellubovich@gmail.com>
2024-03-03 14:40:38 -0700

    Update css link

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // start the express server, serving index.html on port 4200
 
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const app = express();
+const app = express()
 
 // Serve static files from the 'src' directory
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname)))
 
-app.get('/*', function(req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'));
-});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 4200)
